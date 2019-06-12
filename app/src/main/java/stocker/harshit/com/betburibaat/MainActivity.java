2693f1dc58e6ba485434660186a6e2b870750558
harshit <harshit181@gmail.com>
2019-06-12 23:30:08 +0530

    First commit

---
+++ app/src/main/java/stocker/harshit/com/betburibaat/MainActivity.java
@@ -0,0 +1,72 @@
+package stocker.harshit.com.betburibaat;
+
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements Home.OnFragmentInteractionListener ,Stats.OnFragmentInteractionListener,Notification.OnFragmentInteractionListener{
+    private ActionBar toolbar;
+    Fragment fragment;
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    toolbar.setTitle(R.string.title_home);
+                    fragment = new Home();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_dashboard:
+                    toolbar.setTitle(R.string.title_dashboard);
+                    fragment = new Stats();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_notifications:
+                    toolbar.setTitle(R.string.title_notifications);
+                    fragment = new Notification();
+                    loadFragment(fragment);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        toolbar = getSupportActionBar();
+        loadFragment(new Home());
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+}
---
+++ app/src/main/java/stocker/harshit/com/betburibaat/OnFragmentInteractionListener.java
@@ -0,0 +1,4 @@
+package stocker.harshit.com.betburibaat;
+
+public interface OnFragmentInteractionListener {
+}
